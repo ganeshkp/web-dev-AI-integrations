f4738335efeec116c10367b18cc85f2d59f4d759
Ganesh <ganeshkumar.patil@gmail.com>
2019-12-15 19:41:53 +0000

    Committing new files

---
+++ JS/colorChanger/colorChanger.js
@@ -0,0 +1,8 @@
+function backBlue() {
+  document.body.style.backgroundColor="#ccffe6";
+}
+
+function backRed() {
+  //document.body.style.backgroundColor=" #ff1a1a";
+  document.body.style.backgroundColor="green";
+}
