3e0b39084dd8ea335dafc2da67e32cc7d6ae7b0b
Ganesh <ganeshkumar.patil@gmail.com>
2019-12-22 20:56:57 +0000

    Committing new worked files

---
+++ JS/comments/comments.js
@@ -0,0 +1,20 @@
+/*
+This script is for .....
+*/
+
+/*
+The purpose of this fucntion is ....
+*/
+
+function longFunctino() {
+
+
+  regr
+
+  erg
+  ergre
+  gre
+
+  ergre
+
+}
---
+++ JS/tip/tip.js
@@ -0,0 +1,18 @@
+
+
+function calculateTip() {
+  var totalAmount = document.getElementById('billAmount').value;
+  var qualityOfService = document.getElementById('qualityService').value;
+  var numOfPeople = document.getElementById('numPeople').value;
+
+  if (totalAmount === '') {
+    alert('Please enter the total bill amount')
+  } else if (numOfPeople === '') {
+    alert('Please put in the number of people')
+  } else {
+    var tipPerPerson = Math.round((totalAmount*qualityOfService)/numOfPeople);
+    document.getElementById('resultAmount').innerHTML='Tip amount: <br>'+'$ '+tipPerPerson +'<br>Per person';
+
+  }
+
+  }
